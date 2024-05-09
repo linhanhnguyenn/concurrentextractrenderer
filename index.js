79a2f7f762d61fb9788f07a2ed85a4c0fdc8ac3d
linhanhnguyenn <fendergunspun0suy@hotmail.com>
2024-05-09 09:49:49 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function rob(nums) {
-  let prevMax = 0;
-  let currMax = 0;
-  for (const num of nums) {
-    const temp = currMax;
-    currMax = Math.max(prevMax + num, currMax);
-    prevMax = temp;
+function findMin(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] > nums[right]) left = mid + 1;
+    else right = mid;
   }
-  return currMax;
+  return nums[left];
 }
