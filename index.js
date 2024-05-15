75a0c9e5dd8c026bbb5b1bfbadb30a3ba8a0470d
linhanhnguyenn <fendergunspun0suy@hotmail.com>
2024-05-15 12:15:04 +0700

    demo

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-function minStack() {
-  this.stack = [];
-  this.minStack = [];
+function countWords(str) {
+  return str.split(" ").filter((word) => word !== "").length;
 }
-minStack.prototype.push = function (x) {
-  this.stack.push(x);
-  if (
-    this.minStack.length === 0 ||
-    x <= this.minStack[this.minStack.length - 1]
-  )
-    this.minStack.push(x);
-};
-minStack.prototype.pop = function () {
-  if (this.stack.pop() === this.minStack[this.minStack.length - 1])
-    this.minStack.pop();
-};
-minStack.prototype.top = function () {
-  return this.stack[this.stack.length - 1];
-};
-minStack.prototype.getMin = function () {
-  return this.minStack[this.minStack.length - 1];
-};
